9ab5d86f66bb7830c77de5b30cca2d183eae2252
vmzor <vmzor@ya.ru>
2022-07-16 15:11:52 +0300

    change function view_the_log

--- vsearch4web.py
+++ vsearch4web.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(str(dir(req)), res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -33,13 +33,14 @@ def entry_page() -> 'html':
 
 @app.route('/viewlog')
 def view_the_log() -> str:
-    with open ('vsearch.log') as log:
-        contents = log.read()
-    return escape(contents)
+    contents = []
+    with open('vsearch.log') as log:
+            for line in log:
+                contents.append([])
+                for item in line.split('|'):
+                    contents[-1].append(escape(item))
+    return str(contents)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
