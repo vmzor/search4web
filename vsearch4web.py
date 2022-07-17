4c7a5865bb9d0c5fcc522bc2bd16623c74acd0c6
vmzor <vmzor@ya.ru>
2022-07-17 17:23:39 +0300

    generated readable output using html

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
-
+import html
 
 app = Flask(__name__)
 
@@ -32,14 +32,18 @@ def entry_page() -> 'html':
 
 
 @app.route('/viewlog')
-def view_the_log() -> str:
+def view_the_log() -> 'html':
     contents = []
     with open('vsearch.log') as log:
-            for line in log:
-                contents.append([])
-                for item in line.split('|'):
-                    contents[-1].append(escape(item))
-    return str(contents)
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View log',
+                           the_row_titles=titles,
+                           the_data=contents, )
 
 
 if __name__ == '__main__':
