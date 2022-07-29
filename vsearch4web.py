2e6a7eec3c865f8c74761514aeb23dc6f4bb7c4f
vmzor <vmzor@ya.ru>
2022-07-29 13:50:56 +0300

    added db-api for func log_request

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
-import html
 
 app = Flask(__name__)
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+    dbconfig = {'host': '127.0.0.1',
+                'user': 'vsearch',
+                'password': 'vsearchpasswd',
+                'database': 'vsearchlogDB', }
+
+    import mysql.connector
+
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """insert into log
+                (phrase, letters, ip, browser_string,results)
+                values
+                (%s, %s, %s, %s, %s)"""
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res,))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
 @app.route('/search4', methods=['POST'])
